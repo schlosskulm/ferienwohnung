db79a719ae94c5c60272b63ec0f39f8314f444c8
Rafaela Wetzel <schlosskulm.fewo@gmail.com>
2024-11-05 21:29:14 +0000

    Add sweetalert2 functionality for contact form confirmation

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -5,13 +5,33 @@ window.onload = function () {
         event.preventDefault();
         emailjs.sendForm('service_fcm8eeh', 'schlosskulm_template', this)
             .then(function () {
-            <div id="contact-confirmation"></div>
-                let contactConfirmation = document.getElementById("contact-confirmation");
-                contactConfirmation.innerHTML="<p>Danke für deine Nachricht. Wir setzen uns zeitnah mit dir in Verbindung.</p>";
-                /*window.location.href = 'https://schlosskulm.github.io/ferienwohnung/kontakt.html';*/
+                Swal.fire({
+                    icon: "success",
+                    text: "Danke für deine Nachricht! Wir werden uns zeitnah mit dir in Verbindung setzen."
+                });
+                document.getElementById("contact-form").reset();
             }, function (error) {
-                console.log('FAILED...', error);
-                alert('Unfortunately your message has not been sent due to an error. Please try again.');
+                Swal.fire({
+                    icon: "error",
+                    text: "Leider hat es nicht geklappt, die Nachricht zu verschicken. Versuche es später noch einmal."
+                });
+            });
+    });
+
+    document.getElementById('contact-form-en').addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('service_fcm8eeh', 'schlosskulm_english', this)
+            .then(function () {
+                Swal.fire({
+                    icon: "success",
+                    text: "Thank you for your message! We will get in touch with you soon."
+                });
+                document.getElementById("contact-form-en").reset();
+            }, function (error) {
+                Swal.fire({
+                    icon: "error",
+                    text: "Unfortunately there was an error sending your message. Please try again later."
+                });
             });
     });
 };
