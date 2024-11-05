e7c6d8b31a5d9930341db1a08e13b362a48ece9b
Rafaela Wetzel <schlosskulm.fewo@gmail.com>
2024-11-05 19:45:18 +0000

    Change forwarding link to start page

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -5,7 +5,7 @@ window.onload = function () {
         event.preventDefault();
         emailjs.sendForm('service_fcm8eeh', 'schlosskulm_template', this)
             .then(function () {
-                window.location.href = 'https://8000-schlosskulm-ferienwohnu-0gymsjmku15.ws-eu116.gitpod.io/index.html';
+                window.location.href = 'https://schlosskulm.github.io/ferienwohnung/index.html';
             }, function (error) {
                 console.log('FAILED...', error);
                 alert('Unfortunately your message has not been sent due to an error. Please try again.');
